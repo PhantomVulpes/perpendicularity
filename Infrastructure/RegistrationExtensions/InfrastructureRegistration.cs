57f4d77ca5500c931041c24a8f6b3ec04e284ab0
PhantomVulpes <noahgbuss@gmail.com>
2026-04-20 17:32:41 -0500

    Added Zinc Api

--- Infrastructure/RegistrationExtensions/InfrastructureRegistration.cs
+++ Infrastructure/RegistrationExtensions/InfrastructureRegistration.cs
@@ -1,24 +1,38 @@
 using Microsoft.Extensions.DependencyInjection;
 using Vulpes.Electrum.Domain.Data;
 using Vulpes.Electrum.Domain.Mongo;
-using Vulpes.Perpendicularity.Core.Models;
 using Vulpes.Perpendicularity.Infrastructure.Mongo;
+using Infrastructure.Zinc;
 
 namespace Vulpes.Perpendicularity.Infrastructure.RegistrationExtensions;
 
 public static class InfrastructureRegistration
 {
     public static IServiceCollection InjectInfrastructure(this IServiceCollection services) => services
+        .AddZincClient("http://shadesmar:60000/")
         .InjectMongoServices()
         .InjectQueryProviders()
         ;
 
-    public static IServiceCollection InjectMongoServices(this IServiceCollection services) => services
+    private static IServiceCollection InjectMongoServices(this IServiceCollection services) => services
         .AddSingleton<IMongoProvider, MongoProvider>()
         .AddTransient(typeof(IModelRepository<>), typeof(MongoRepository<>))
         ;
 
     private static IServiceCollection InjectQueryProviders(this IServiceCollection services) => services
-        .AddTransient<IQueryProvider<RegisteredUser>, MongoQueryProvider<RegisteredUser>>()
+        .AddTransient<IQueryProvider<Core.Models.RegisteredUser>, MongoQueryProvider<Core.Models.RegisteredUser>>()
         ;
+
+    private static IServiceCollection AddZincClient(this IServiceCollection services, string baseUrl)
+    {
+        services.AddHttpClient();
+        services.AddTransient<IZincClient>(serviceProvider =>
+        {
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var httpClient = httpClientFactory.CreateClient();
+            return new ZincClient(baseUrl, httpClient);
+        });
+
+        return services;
+    }
 }
