bd1e89364a27df344400bef0c1777f4ffd19f3ed
PhantomVulpes <noahgbuss@gmail.com>
2026-04-20 16:59:14 -0500

    Response gets alphabetized

--- Api/ResponseModels/DirectoryContentsResponse.cs
+++ Api/ResponseModels/DirectoryContentsResponse.cs
@@ -4,5 +4,11 @@ namespace Vulpes.Perpendicularity.Api.ResponseModels;
 
 public record DirectoryContentsResponse(IEnumerable<string> Directories, IEnumerable<string> Files)
 {
-    public static DirectoryContentsResponse FromDirectoryContents(DirectoryContents directoryContents) => new(directoryContents.Directories, directoryContents.Files);
+    public static DirectoryContentsResponse FromDirectoryContents(DirectoryContents directoryContents)
+    {
+        var sortedDirectories = directoryContents.Directories.OrderBy(d => d).ToList();
+        var sortedFiles = directoryContents.Files.OrderBy(f => f).ToList();
+
+        return new(sortedDirectories, sortedFiles);
+    }
 }
